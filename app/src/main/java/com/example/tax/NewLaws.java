8eb5103354dfaeddca29d7ea3658de7508b9cc43
Zinnia <zinnia2563@gmail.com>
2020-01-13 23:07:05 +0600

    taxassist xml

---
+++ app/src/main/java/com/example/tax/NewLaws.java
@@ -0,0 +1,14 @@
+package com.example.tax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewLaws extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_laws);
+    }
+}
---
+++ app/src/main/java/com/example/tax/expense_2.java
@@ -0,0 +1,14 @@
+package com.example.tax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class expense_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_2);
+    }
+}
